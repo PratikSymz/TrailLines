32ced7aee9e3d9c2be8c1ad4dda2fa6686b2e999
Amit Pai <pai.ami@northeastern.edu>
2022-07-19 14:17:24 -0700

    Gradle issues

--- app/src/main/java/com/neu/madcourse/mad_team4_finalproject/LoginActivity.java
+++ app/src/main/java/com/neu/madcourse/mad_team4_finalproject/LoginActivity.java
@@ -4,7 +4,9 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Context;
 import android.os.Bundle;
+import android.view.View;
 
+import com.google.android.material.textfield.TextInputEditText;
 import com.neu.madcourse.mad_team4_finalproject.databinding.ActivityLoginBinding;
 
 public class LoginActivity extends AppCompatActivity {
@@ -13,6 +15,8 @@ public class LoginActivity extends AppCompatActivity {
 
     /* The Activity context */
     private Context mContext;
+    private TextInputEditText textEmailId, textPassword;
+    private String email, password;
 
     /* The Activity layout view binding reference */
     private ActivityLoginBinding mBinding;
@@ -28,5 +32,28 @@ public class LoginActivity extends AppCompatActivity {
         mBinding = ActivityLoginBinding.inflate(getLayoutInflater());
         // Set the layout root view
         setContentView(mBinding.getRoot());
+        mBinding.someText.setText("Hey!");
+        /**
+         * need to do mbinding to initialize the text input
+         */
+    }
+
+
+    /**
+     * @return none
+     * @param v
+     * This is the button login method
+     */
+    public void loginButton(View v){
+       //email =  mbinding.id.getText().toString().trim()
+        // password =  mbinding.id.getText().toString().trim()
+        if (email.equals("")){
+            //mbinding.id.setError(getString(R.string.empty_email)
+        }
+        else if (password.equals("")){
+            //mbinding.id.setError(getString(R.string.empty_password)
+        } else{
+
+        }
     }
 }

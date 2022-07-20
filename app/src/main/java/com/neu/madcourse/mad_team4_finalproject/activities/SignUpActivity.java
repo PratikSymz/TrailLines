7ade7063ba9b85f8b82d71bf9490894df3c0ce54
Pratik Budhiraja <pratikbudhiraja@gmail.com>
2022-07-20 00:54:28 -0400

    Minor corrections

--- app/src/main/java/com/neu/madcourse/mad_team4_finalproject/activities/SignUpActivity.java
+++ app/src/main/java/com/neu/madcourse/mad_team4_finalproject/activities/SignUpActivity.java
@@ -25,7 +25,6 @@ public class SignUpActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_sign_up);
 
         // Set the activity context
         mContext = this;

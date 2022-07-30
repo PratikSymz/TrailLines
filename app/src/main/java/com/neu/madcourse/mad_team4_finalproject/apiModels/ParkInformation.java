c747daeede8fd9b37625b82af9277c84f92755cc
Amit Pai <pai.ami@northeastern.edu>
2022-07-29 21:18:54 -0700

    API model updates

---
+++ app/src/main/java/com/neu/madcourse/mad_team4_finalproject/apiModels/ParkInformation.java
@@ -0,0 +1,24 @@
+package com.neu.madcourse.mad_team4_finalproject.apiModels;
+
+public class ParkInformation {
+    public String fullName;
+    public String latLong;
+    public String latitude;
+    public String longitude;
+
+    public String getFullName() {
+        return fullName;
+    }
+
+    public String getLatLong() {
+        return latLong;
+    }
+
+    public String getLatitude() {
+        return latitude;
+    }
+
+    public String getLongitude() {
+        return longitude;
+    }
+}

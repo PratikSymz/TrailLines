95527a6ef80f8550e5a0b42ecc8cb925fdd83bb3
Amit Pai <pai.ami@northeastern.edu>
2022-07-31 22:19:22 -0700

    Minor Gradle Updates

--- app/src/main/java/com/neu/madcourse/mad_team4_finalproject/AppMainScreen/MainScreen.java
+++ app/src/main/java/com/neu/madcourse/mad_team4_finalproject/AppMainScreen/MainScreen.java
@@ -5,12 +5,16 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.os.Bundle;
 
 import com.neu.madcourse.mad_team4_finalproject.R;
+import com.neu.madcourse.mad_team4_finalproject.databinding.ActivityMainScreenBinding;
 
 public class MainScreen extends AppCompatActivity {
+
+    private ActivityMainScreenBinding mBinding;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main_screen);
+        mBinding = ActivityMainScreenBinding.inflate(getLayoutInflater());
+        setContentView(mBinding.getRoot());
     }
 }

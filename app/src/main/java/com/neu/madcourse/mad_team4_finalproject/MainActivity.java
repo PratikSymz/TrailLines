6204d78019d906cbc2abe7f659af568cfbc77643
Nishtha Goswami <nishthagoswami@Nishthas-MBP.attlocal.net>
2022-08-07 18:10:50 -0500

    Clean up for master

--- app/src/main/java/com/neu/madcourse/mad_team4_finalproject/MainActivity.java
+++ app/src/main/java/com/neu/madcourse/mad_team4_finalproject/MainActivity.java
@@ -3,8 +3,6 @@ package com.neu.madcourse.mad_team4_finalproject;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
-import android.view.View;
-import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -12,40 +10,5 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-
-        Button mBottton = findViewById(R.id.button);
-        mBottton.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-//                showBottomSheetDialog();
-                FilterBottomSheetDialog bottomSheet = new FilterBottomSheetDialog();
-                bottomSheet.show(getSupportFragmentManager(), bottomSheet.getTag());
-            }
-        });
-
-
-    }
-
-    private void showBottomSheetDialog() {
-
-
-//        final BottomSheetDialog bottomSheetDialog = new BottomSheetDialog(this);
-//        View sheetView;
-//        sheetView = getLayoutInflater().inflate(R.layout.filter_screen,null);
-//
-//        bottomSheetDialog.setContentView(sheetView);
-//
-//        bottomSheetDialog.setOnShowListener(new DialogInterface.OnShowListener() {
-//            @Override
-//            public void onShow(DialogInterface dialog) {
-//                BottomSheetDialog d = (BottomSheetDialog) dialog;
-//
-//                FrameLayout bottomSheet = (FrameLayout) d.findViewById(com.google.android.material.R.id.design_bottom_sheet);
-//                BottomSheetBehavior.from(bottomSheet).setState(BottomSheetBehavior.STATE_EXPANDED);
-//            }
-//        });
-//        bottomSheetDialog.show();
-
-
     }
 }
--- app/src/main/java/com/neu/madcourse/mad_team4_finalproject/fragments/FilterBottomSheetDialog.java
+++ app/src/main/java/com/neu/madcourse/mad_team4_finalproject/fragments/FilterBottomSheetDialog.java
@@ -1,20 +1,14 @@
-package com.neu.madcourse.mad_team4_finalproject;
+package com.neu.madcourse.mad_team4_finalproject.fragments;
 
 import android.app.Dialog;
 import android.os.Bundle;
-import android.view.LayoutInflater;
 import android.view.View;
-import android.view.ViewGroup;
-import android.widget.ImageButton;
 import android.widget.Toast;
-
-import androidx.annotation.Nullable;
-import androidx.databinding.DataBindingUtil;
-import androidx.databinding.ViewDataBinding;
 
 import com.google.android.material.bottomsheet.BottomSheetBehavior;
 import com.google.android.material.bottomsheet.BottomSheetDialog;
 import com.google.android.material.bottomsheet.BottomSheetDialogFragment;
+import com.neu.madcourse.mad_team4_finalproject.R;
 import com.neu.madcourse.mad_team4_finalproject.databinding.FilterScreenBinding;
 
 public class FilterBottomSheetDialog extends BottomSheetDialogFragment {
@@ -31,8 +25,6 @@ public class FilterBottomSheetDialog extends BottomSheetDialogFragment {
         bottomSheet.setContentView(view);
         bottomSheetBehavior = BottomSheetBehavior.from((View) (view.getParent()));
         bottomSheetBehavior.setState(BottomSheetBehavior.STATE_EXPANDED);
-
-//        ImageButton closeFilterScreen = (ImageButton) view.findViewById(R.id.filter_close);
         mBinding.filterClose.setOnClickListener(v -> {
             Toast.makeText(getActivity(),
                             "Closed!!", Toast.LENGTH_SHORT)

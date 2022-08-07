3a050b11c65d8443be5a5bebf1c669b4a4cb5d6b
Nishtha Goswami <nishthagoswami@Nishthas-MBP.attlocal.net>
2022-08-06 22:01:34 -0500

    Filter Screen layout

---
+++ app/src/main/java/com/neu/madcourse/mad_team4_finalproject/ExploreScreen.java
@@ -0,0 +1,14 @@
+package com.neu.madcourse.mad_team4_finalproject;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class ExploreScreen extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_explore_screen);
+    }
+}
--- app/src/main/java/com/neu/madcourse/mad_team4_finalproject/MainActivity.java
+++ app/src/main/java/com/neu/madcourse/mad_team4_finalproject/MainActivity.java
@@ -3,6 +3,11 @@ package com.neu.madcourse.mad_team4_finalproject;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.LinearLayout;
+
+import com.google.android.material.bottomsheet.BottomSheetDialog;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +15,27 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        Button mBottton = findViewById(R.id.button);
+        mBottton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                showBottomSheetDialog();
+            }
+        });
+    }
+
+    private void showBottomSheetDialog() {
+
+        final BottomSheetDialog bottomSheetDialog = new BottomSheetDialog(this);
+        bottomSheetDialog.setContentView(R.layout.bottom_sheet_filter_screen);
+
+        LinearLayout copy = bottomSheetDialog.findViewById(R.id.copyLinearLayout);
+        LinearLayout share = bottomSheetDialog.findViewById(R.id.shareLinearLayout);
+        LinearLayout upload = bottomSheetDialog.findViewById(R.id.uploadLinearLayout);
+        LinearLayout download = bottomSheetDialog.findViewById(R.id.download);
+        LinearLayout delete = bottomSheetDialog.findViewById(R.id.delete);
+
+        bottomSheetDialog.show();
     }
 }

27e662fe2970c05d311b47a27af6a8a675b370ad
Nishtha Goswami <nishthagoswami@Nishthas-MBP.attlocal.net>
2022-08-06 22:01:34 -0500

    Filter Screen layout

--- app/src/main/java/com/neu/madcourse/mad_team4_finalproject/MainActivity.java
+++ app/src/main/java/com/neu/madcourse/mad_team4_finalproject/MainActivity.java
@@ -1,6 +1,11 @@
 package com.neu.madcourse.mad_team4_finalproject;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.LinearLayout;
+
+import com.google.android.material.bottomsheet.BottomSheetDialog;
 
 import androidx.appcompat.app.AppCompatActivity;
 
@@ -9,6 +14,29 @@ public class MainActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+
         setContentView(R.layout.activity_main_screen);
+
+        Button mBottton = findViewById(R.id.button);
+        mBottton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                showBottomSheetDialog();
+            }
+        });
+    }
+
+    private void showBottomSheetDialog() {
+
+        final BottomSheetDialog bottomSheetDialog = new BottomSheetDialog(this);
+        bottomSheetDialog.setContentView(R.layout.bottom_sheet_filter_screen);
+
+        LinearLayout copy = bottomSheetDialog.findViewById(R.id.copyLinearLayout);
+        LinearLayout share = bottomSheetDialog.findViewById(R.id.shareLinearLayout);
+        LinearLayout upload = bottomSheetDialog.findViewById(R.id.uploadLinearLayout);
+        LinearLayout download = bottomSheetDialog.findViewById(R.id.download);
+        LinearLayout delete = bottomSheetDialog.findViewById(R.id.delete);
+
+        bottomSheetDialog.show();
     }
 }
